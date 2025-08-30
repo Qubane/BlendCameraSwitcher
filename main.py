14c221ab4fdda8317740871997ddd59f77261579
UltraQbik <no1skill@yandex.ru>
2025-08-30 22:12:40 +0300

    Add argument parser

--- main.py
+++ main.py
@@ -3,19 +3,36 @@ Main file
 """
 
 
+import argparse
+
+
 class Application:
     """
     Main application class
     """
 
     def __init__(self):
-        ...
+        self.blender_file: str = ""
 
     def _parse(self):
-        ...
+        """
+        Parse CLI arguments
+        """
+
+        parser = argparse.ArgumentParser(prog="Blender Camera Switcher")
+
+        parser.add_argument("-i", "--input",
+                            help="blender input file",
+                            required=True)
+
+        args = parser.parse_args()
+
+        self.blender_file = args.input
 
     def run(self):
-        ...
+        """
+        Runs the application
+        """
 
 
 def main():
