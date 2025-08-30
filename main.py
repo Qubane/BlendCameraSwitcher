6f774e72ea4d2eefeda6f10b971a8df10f74674a
UltraQbik <no1skill@yandex.ru>
2025-08-31 02:00:17 +0300

    Something working

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ class Application:
         self.blender_file: str = ""
         self.output_directory: str = ""
 
+        self.scene_cameras: list = []
+
         self.config: dict[str, dict] | None = None
 
     def _parse(self):
@@ -37,7 +39,7 @@ class Application:
         args = parser.parse_args()
 
         self.blender_file = args.input
-        self.output_directory = os.path.dirname(args.output)
+        self.output_directory = args.output
         if not os.path.isdir(self.output_directory):
             os.makedirs(self.output_directory)
 
@@ -65,17 +67,35 @@ class Application:
         bpy.ops.wm.open_mainfile(filepath=self.blender_file)
 
         # read cameras
-        scene_cameras = [obj for obj in bpy.data.objects if obj.type == 'CAMERA']
-        scene_camera_names = [camera.name for camera in scene_cameras]
+        self.scene_cameras = [obj for obj in bpy.data.objects if obj.type == 'CAMERA']
+        scene_camera_names = [camera.name for camera in self.scene_cameras]
 
         # ensure the cameras are present in config
         for config_camera in self.config.keys():
             if config_camera not in scene_camera_names:
-                raise KeyError
+                raise KeyError(f"Camera '{config_camera}' not found!")
 
         # make render
         for config_camera, config_data in self.config.items():
-            path = config_data["path"] if config_data["path"] is not None else config_camera
+            path = config_data.get("path")
+
+            # if path was not provided
+            if path is None:
+                path = os.path.join(self.output_directory, config_camera)
+
+            # if path is relative
+            elif path[:2].replace("\\", "/") == "//":
+                path = os.path.join(self.output_directory, path[2:])
+
+            # make sure the path is absolute
+            path = os.path.join(os.getcwd(), path)
+
+            # assign path to blender output
+            bpy.context.scene.render.filepath = os.path.join(path, "f_")
+
+            # go through ranges and render them
+            for frame_range in config_data["ranges"]:
+                self.render_camera(config_camera, frame_range[0], frame_range[1])
 
     def render_camera(self, camera_name: str, range_start: int, range_end: int):
         """
@@ -84,6 +104,17 @@ class Application:
         :param range_start: frame starting range
         :param range_end: frame ending range
         """
+
+        # update frame ranges
+        bpy.context.scene.frame_start = range_start
+        bpy.context.scene.frame_end = range_end
+
+        # switch camera
+        camera = [camera for camera in self.scene_cameras if camera.name == camera_name][0]
+        bpy.context.scene.camera = camera
+
+        # update view
+        bpy.context.view_layer.update()
 
 
 def main():
